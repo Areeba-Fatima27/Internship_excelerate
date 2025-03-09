89b358e2fe565c4e8bdab11e09d5536f631f6a0b
Areeba-Fatima27 <areebafatima721@gmail.com>
2025-03-09 11:43:40 +0500

    upload file

---
+++ LearnerOpportunity_cleaning.sql
@@ -0,0 +1,46 @@
+CREATE TABLE Learner_opportunity(
+    enrollment_id VARCHAR(150),
+	learner_id	VARCHAR(150),
+	assigned_cohort	VARCHAR(50),
+	apply_date	TIMESTAMP with TIME ZONE,
+	status	VARCHAR(20))		
+
+ALTER TABLE "learner_opportunity" RENAME TO learner_opportunity
+
+COPY public."learner_opportunity" (enrollment_id, learner_id, assigned_cohort, apply_date, status)
+FROM 'C:\One Drive\OneDrive - Higher Education Commission\Apps\LearnerOpportunity_Raw-ar.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL 'NULL');
+
+select*
+from learner_opportunity
+
+ALTER TABLE "learner_opportunity"
+ALTER COLUMN apply_date TYPE TIMESTAMP
+
+select distinct count(*)
+from learner_opportunity
+
+select distinct count(enrollment_id) as no_ofenrollment_id
+from learner_opportunity
+
+select distinct count(learner_id) as no_oflearner_id
+from learner_opportunity
+
+select distinct count(assigned_cohort) as no_ofassigned_cohort
+from learner_opportunity	
+
+select distinct assigned_cohort, count(assigned_cohort) as total_assigned_cohort
+from learner_opportunity	
+group by assigned_cohort
+Order by total_assigned_cohort desc
+
+select distinct count(apply_date) as no_ofapply_date
+from learner_opportunity	
+
+select distinct apply_date, count(apply_date) as total_apply_date
+from learner_opportunity	
+group by apply_date
+Order by total_apply_date desc
+
+select distinct count(status) as no_ofstatus
+from learner_opportunity	
---
+++ cognito data cleaning.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Cognitodata (
+    user_id VARCHAR(150),
+    email VARCHAR(150),
+    gender VARCHAR(150),
+    UserCreateDate TIMESTAMP,  
+    UserLastModifiedDate TIMESTAMP,  
+    birthdate DATE,
+    city VARCHAR(150),
+    zip VARCHAR(50),
+    state VARCHAR(50)
+);
+
+COPY public.cognitodata (user_id, email, gender, UserCreateDate, UserLastModifiedDate, birthdate,city,zip,state)
+FROM 'C:\One Drive\OneDrive - Higher Education Commission\Apps\Cognito_Raw2 -ar.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL 'NULL');
+
+select *
+from cognitodata
+
+select distinct count(user_id) as no_ofuser_id
+from cognitodata
+
+SELECT user_id, COUNT (*) as Count
+FROM cognitodata
+group by user_id
+Having COUNT (user_id) > 1
+
+
+select email
+from cognitodata
+
+select distinct gender, count(gender)
+from cognitodata
+group by gender
+
+select distinct city, count(city) as total_cities
+from cognitodata
+group by city
+Order by total_cities desc
+
+select distinct zip, count(zip) as total_zip
+from cognitodata
+group by zip
+Order by total_zip desc
+
+select distinct state, count(state) as total_state
+from cognitodata
+group by state
+Order by total_state desc
